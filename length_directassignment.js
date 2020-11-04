e19670213f01d9e10fb835973efbea0cfde19230
Jamie <jlp0328@gmail.com>
2020-11-04 18:27:14 -0500

    Beginning State:01_02

---
+++ length_directassignment.js
@@ -0,0 +1,4 @@
+const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+//Use the length property to determine how many objects are in the array
+
+//Using direct assignment, add 'snake' to the array in index position 2.
