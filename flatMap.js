33457301326b6df9c54f1e6d2b2cf8262ed98f1f
Jamie <jlp0328@gmail.com>
2020-11-04 21:28:31 -0500

    Beginning State: 05_04

---
+++ flatMap.js
@@ -0,0 +1,7 @@
+const wordJumble = ['shampoo', 'conditioner', 'soap'];
+
+//You want to create a word jumble which requires you to split
+//the following array of words into individual letters. You want
+//a new array, that is flattened to a single level.
+
+//Hint: You will need to use the string method .split()
