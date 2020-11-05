30a56bdbb9791799748c928e6d02156ceeb9bc5d
Jamie <jlp0328@gmail.com>
2020-11-04 21:05:56 -0500

    Beginning State: 02_02

---
+++ reverse.js
@@ -0,0 +1,6 @@
+const condiments = ['ketchup', 'mustard', 'relish', 'mayo'];
+// Use reverse to change the order of this array.
+
+const employees = ['Adams', 'Carter', 'Davis', 'Fraklin', 'Zappa'];
+// Use reverse to change the order of this array,
+// but keep the original array intact.
