afd728a6121520ed7f2206d1b15943fb40852a1d
Jamie <jlp0328@gmail.com>
2020-11-04 21:17:02 -0500

    Beginning State: 04_01

---
+++ push_pop.js
@@ -0,0 +1,6 @@
+const drinks = ['coffee', 'tea', 'soda', 'water'];
+//How do we add "wine" to the array of drinks?
+
+const seasons = ['spring', 'summer', 'fall', 'winter'];
+//How do we remove the last season from the seasons array?
+//Store the element that is removed in a variable.
