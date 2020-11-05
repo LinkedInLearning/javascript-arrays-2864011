c33f7d5bb3ccb61d4d7317a3f93dc924bdd76a2e
Jamie <jlp0328@gmail.com>
2020-11-04 21:12:35 -0500

    Ending State: 03_01

---
+++ includes_solution.js
@@ -0,0 +1,5 @@
+const careers = ["teacher", "doctor", "engineer", "nurse", "scientist"];
+
+//Does the careers array include a nurse?
+const nurseIncluded = careers.includes("nurse");
+console.log("Is a nurse included in the array", nurseIncluded)
