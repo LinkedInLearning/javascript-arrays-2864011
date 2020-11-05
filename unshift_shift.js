5a75c800f090d06aa7a702868726e0ecfdae5042
Jamie <jlp0328@gmail.com>
2020-11-04 21:18:40 -0500

    Beginning State: 04_02

---
+++ unshift_shift.js
@@ -0,0 +1,6 @@
+const ages = [18, 33, 71, 29, 65, 62];
+//How do we add 36 to the array of ages?
+
+const holidays = ['Labor Day', 'Christmas', 'Hanakkuh', 'Independence Day'];
+//How do we remove the fist item from the holidays array?
+//Store the element that is removed in a variable.
