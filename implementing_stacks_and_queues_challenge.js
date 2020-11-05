1efa3a338224a3c89d28164926737859427da886
Jamie <jlp0328@gmail.com>
2020-11-04 21:19:56 -0500

    Challenge: 04_03

---
+++ implementing_stacks_and_queues_challenge.js
@@ -0,0 +1,23 @@
+//Push, Pop, Unshift, and Shift
+
+const foods = [
+  { food: 'raspberries', type: 'fruit' },
+  { food: 'orange', type: 'fruit' },
+  { food: 'broccoli', type: 'vegetable' },
+  { food: 'quinoa', type: 'grain' },
+];
+const blackBeans = { food: 'black beans', type: 'legume' };
+const chiaSeeds = { food: 'chia seeds', type: 'seed' };
+
+//Challenge Question #1
+//Remove the last item in the foods array.
+
+//Challenge Question #2
+//Remove the first item in the foods array.
+
+//Challenge Question #3
+//Add the variable blackBeans to the beginning of the foods array.
+
+//Challenge Question #4
+//Add the variable chiaSeeds to the end of the array.
+//What does your final array look like?
