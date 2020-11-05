d99f11370804884b5b334dbfc5670af9c6396081
Jamie <jlp0328@gmail.com>
2020-11-04 21:01:53 -0500

    Beginning State: 01_03

---
+++ forEach.js
@@ -0,0 +1,6 @@
+const scores = [50, 60, 70, 80, 90, 100];
+
+const logScore = (score) => console.log('Score Over 65', score);
+
+// Iterate over the scores array using forEach.
+// Execute the logScores
