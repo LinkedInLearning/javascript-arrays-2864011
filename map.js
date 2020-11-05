60136df756b18300898c03ac53a4844e2a65e548
Jamie <jlp0328@gmail.com>
2020-11-04 21:23:14 -0500

    Beginning State: 05_01

---
+++ map.js
@@ -0,0 +1,7 @@
+const friends = [
+  { firstname: 'Jane', lastname: 'Doe' },
+  { firstname: 'John', lastname: 'Smith' },
+  { firstname: 'Alex', lastname: 'Trebek' },
+];
+
+//Create a new array that includes only the full name (firstname + lastname) of each friend.
