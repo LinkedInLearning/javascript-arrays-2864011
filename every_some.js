866f601630abe79a009c115b5b467c0436a0fbcb
Jamie <jlp0328@gmail.com>
2020-11-04 21:13:52 -0500

    Beginning State: 03_02

---
+++ every_some.js
@@ -0,0 +1,14 @@
+const states = [
+  'California',
+  'New York',
+  'New Jersey',
+  'Alaska',
+  'New Mexico',
+  'Montana',
+];
+
+//Use this handy string method to find the solution: startsWith("New")
+
+//Do some of the states in the array start with "New"?
+
+//Does every state in the array start with "New"?
