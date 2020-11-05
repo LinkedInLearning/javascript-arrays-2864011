a51d818f17cfbf321d662ec92f889a16bad85467
Jamie <jlp0328@gmail.com>
2020-11-04 21:29:55 -0500

    Challenge: 05_05

---
+++ advanced_methods_challenge.js
@@ -0,0 +1,37 @@
+//Map, Filter, Reduce, and FlatMap
+const movies = [
+  {
+    title: 'Avatar',
+    year: '2009',
+    rating: 'PG-13',
+  },
+  {
+    title: 'I Am Legend',
+    year: '2007',
+    rating: 'PG-13',
+  },
+  {
+    title: '300',
+    year: '2006',
+    rating: 'R',
+  },
+  {
+    title: 'The Avengers',
+    year: '2012',
+    rating: 'PG-13',
+  },
+  {
+    title: 'The Wolf of Wall Street',
+    year: '2013',
+    rating: 'R',
+  },
+];
+
+//Challenge Question #1
+//Create a new array that just contains the movie titles.
+
+//Challenge Question #2
+//Create a new array that contains only those movies that are PG-13.
+
+//Challenge Question #3
+//Determine how many of the movies are rating R.
