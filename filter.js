73be7cb32e73f55490f23bdef5923266845cbbc3
Jamie <jlp0328@gmail.com>
2020-11-04 21:25:18 -0500

    Beginning State: 05_02

---
+++ filter.js
@@ -0,0 +1,10 @@
+const groceries = [
+  { item: 'apples', type: 'fruit' },
+  { item: 'cereal', type: 'pantry' },
+  { item: 'pork chops', type: 'meat' },
+  { item: 'bananas', type: 'fruit' },
+  { item: 'broccoli', type: 'vegetable' },
+];
+
+//Filter the following array to include only those grocery items
+//that are fruits.
