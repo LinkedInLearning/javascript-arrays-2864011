11dd0258cea0359fcd9388fdf489b89c47b161a3
Jamie <jlp0328@gmail.com>
2020-11-04 21:09:59 -0500

    Challenge: 02_04

---
+++ working_with_ordered_data_challenge.js
@@ -0,0 +1,17 @@
+//Sort, Reverse, Find, FindIndex
+const students = [
+  { name: 'John', grade: 75 },
+  { name: 'Jane', grade: 93 },
+  { name: 'Samantha', grade: 90 },
+  { name: 'Michael', grade: 94 },
+];
+
+//Challenge Question #1
+//Sort the array of students based on their grade,
+//in descending order(largest to smallest).
+
+//Challenge Question #2
+//After sorting the array, reverse the order of the array.
+
+//Challenge Question #3
+//Find a student in the array who has a grade over 90.
