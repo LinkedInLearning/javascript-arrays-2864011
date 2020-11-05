ef8382ad914bdb6defadf41e5a53c4b5ab772897
Jamie <jlp0328@gmail.com>
2020-11-04 21:04:08 -0500

    Beginning State: 02_01

---
+++ sort.js
@@ -0,0 +1,7 @@
+//Sort the following array alphabetically
+const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+
+const grades = [88, 94, 72, 99, 53, 77];
+//Sort the following array from highest to lowest
+
+//Sort the following array from lowest to highest
