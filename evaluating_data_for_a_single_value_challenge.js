4f9af0f1deadf78d06344ec39b1be48bfa3305a7
Jamie <jlp0328@gmail.com>
2020-11-04 21:15:12 -0500

    Challenge: 03_03

---
+++ evaluating_data_for_a_single_value_challenge.js
@@ -0,0 +1,11 @@
+//Includes, Some, and Every
+const bowlingScores = [154, 204, 300, 184, 286];
+
+//Challenge Question #1
+//Does the array of bowling scores include a 300?
+
+//Challenge Question #2
+//Are some of the scores under 150?
+
+//Challenge Question #3
+//Is every score an even number?
